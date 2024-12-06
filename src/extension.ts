954338e83d3acbf15e32ed86c287ce46cfbfe350
Fran Aguilera <fraguile@microsoft.com>
2024-12-06 18:05:47 -0500

    Dynamically switch gitignore logic.

--- src/extension.ts
+++ src/extension.ts
@@ -12,8 +12,25 @@ export function activate(context: vscode.ExtensionContext): void {
   const config = vscode.workspace.getConfiguration('marktree');
   const gitignoreEnabled = config.get<boolean>('gitignore', true);
   const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
-
   initializeIgnore(workspaceFolder, gitignoreEnabled);
+
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeConfiguration(e => {
+      if (e.affectsConfiguration('marktree.gitignore')) {
+        const updatedConfig = vscode.workspace.getConfiguration('marktree');
+        const updatedGitignoreEnabled = updatedConfig.get<boolean>(
+          'gitignore',
+          true
+        );
+        const updatedWorkspaceFolder =
+          vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+        initializeIgnore(updatedWorkspaceFolder, updatedGitignoreEnabled);
+        outputChannel.appendLine(
+          `marktree.gitignore changed to ${updatedGitignoreEnabled}. Re-initialized ignore logic.`
+        );
+      }
+    })
+  );
 
   registerCommands(context, outputChannel);
 }
