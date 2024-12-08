4076620eed66a72826e7d3c22a63dfc7e7f34e9e
Fran Aguilera <fraguile@microsoft.com>
2024-12-08 16:50:48 -0500

    v0.0.6 - Add lockfiles to default ignore.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,11 @@
 export const DEFAULT_GITIGNORE = true;
 export const DEFAULT_IGNORE_FILES = ['.gitignore'];
 export const DEFAULT_IGNORE_BINARY = true;
-export const DEFAULT_ADDITIONAL_IGNORES = ['.gitignore'];
+export const DEFAULT_ADDITIONAL_IGNORES = [
+  '.gitignore',
+  'yarn.lock',
+  'pnpm-lock.yaml',
+  'package-lock.json',
+];
 export const DEFAULT_SHOW_COPYING_MSG = true;
 export const DEFAULT_SHOW_COPIED_MSG = false;
