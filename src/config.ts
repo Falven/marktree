4d1c64cc0206ec390a4e7d8ea52664df70a07b30
Fran Aguilera <fraguile@microsoft.com>
2024-12-09 19:12:26 -0500

    v0.1.3 - Add defaults to config

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,25 @@ export const DEFAULT_GITIGNORE = true;
 export const DEFAULT_IGNORE_FILES = ['.gitignore'];
 export const DEFAULT_IGNORE_BINARY = true;
 export const DEFAULT_ADDITIONAL_IGNORES = [
+  '.git',
   '.gitignore',
+  '.vscode/',
+  '.idea/',
+  '.settings/',
+  '.vs/',
   'yarn.lock',
   'pnpm-lock.yaml',
   'package-lock.json',
+  'README.md',
+  'LICENSE',
+  'LICENSE.md',
+  'CHANGELOG.md',
+  'CODE_OF_CONDUCT.md',
+  'CONTRIBUTING.md',
+  'SECURITY.md',
+  '.editorconfig',
+  '.gitattributes',
+  '.mailmap',
 ];
 export const DEFAULT_SHOW_COPYING_MSG = false;
 export const DEFAULT_SHOW_COPIED_MSG = true;
