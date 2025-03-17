47ba2c90a29b42f4065e2a9353110f3cba0162d0
Fran Aguilera <fraguile@microsoft.com>
2025-03-16 21:07:52 -0400

    Refactor copyActiveFileMd command and update version
    - Removed redundant configuration and UI messages in copy-active-file command.
    - Simplified logging to only report errors and workflow completion.
    - Bumped version from 0.2.3 to 0.2.4.

--- src/commands/copy-active-file.ts
+++ src/commands/copy-active-file.ts
@@ -1,24 +1,9 @@
 import * as vscode from 'vscode';
-import {
-  DEFAULT_SHOW_COPIED_MSG,
-  DEFAULT_SHOW_COPYING_MSG,
-} from '../config.js';
 import { copyMdFiles } from './copy-files.js';
 
 export const copyActiveFileMd =
   (context: vscode.ExtensionContext, outputChannel: vscode.OutputChannel) =>
   async () => {
-    const config = vscode.workspace.getConfiguration('marktree');
-    const showCopyingMsg = config.get<boolean>(
-      'showCopyingMessage',
-      DEFAULT_SHOW_COPYING_MSG
-    );
-    let message = 'Copying active file contents to clipboard as Markdown.';
-    outputChannel.appendLine(message);
-    if (showCopyingMsg) {
-      vscode.window.showInformationMessage(message);
-    }
-
     const activeEditor = vscode.window.activeTextEditor;
     if (!activeEditor) {
       vscode.window.showErrorMessage('No active file to copy.');
@@ -26,15 +11,4 @@ export const copyActiveFileMd =
     }
     const fileUri = activeEditor.document.uri;
     await copyMdFiles(context, outputChannel)(fileUri);
-
-    const showCopiedMsg = config.get<boolean>(
-      'showCopiedMessage',
-      DEFAULT_SHOW_COPIED_MSG
-    );
-
-    message = 'Active file copied to clipboard as Markdown.';
-    outputChannel.appendLine(message);
-    if (showCopiedMsg) {
-      vscode.window.showInformationMessage(message);
-    }
   };
